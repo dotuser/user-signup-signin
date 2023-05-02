a17a0350efc094388ef2b5ee93ee199dd61237e5
dotuser <ahmedhassaan@tutanota.com>
2023-05-02 15:37:16 +0500

    User Registration

--- app.js
+++ app.js
@@ -7,32 +7,72 @@ const env = require("dotenv");
 const app = express();
 env.config();
 
-app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
+//////////////////////////////////////////// MongoDB Connection ////////////////////////////////////////////
+
+mongoose.connect(process.env.URI, { useNewUrlParser: true });
+
+//////////////////////////////////////////// User Schema && Model ////////////////////////////////////////////
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: String,
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
 //////////////////////////////////////////// HOME Route ////////////////////////////////////////////
-app.route("/").get((req, res) => {
+
+app.get("/", (req, res) => {
   res.render("home");
 });
 
 //////////////////////////////////////////// LOGIN Route ////////////////////////////////////////////
+
 app.route("/login").get((req, res) => {
   res.render("login");
 });
 
 //////////////////////////////////////////// REGISTER Route ////////////////////////////////////////////
-app.route("/register").get((req, res) => {
-  res.render("register");
-});
+
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    let reg = "Registration";
+    let log = "Login";
+
+    const user = new User({
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    user.save();
+
+    res.render("success", {
+      reg: reg,
+      log: log,
+    });
+  });
 
 //////////////////////////////////////////// SUCCESS Route ////////////////////////////////////////////
-app.route("/success").get((req, res) => {
+
+app.get("/success", (req, res) => {
   res.render("success");
 });
 
 //////////////////////////////////////////// ERROR Route ////////////////////////////////////////////
-app.route("/error").get((req, res) => {
+
+app.get("/error", (req, res) => {
   res.render("error");
 });
 
